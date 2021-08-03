d7ad94fae868c3009349d4eb6c3b9bc641c5c6c6
LuizTkaczyk <luiz8907@hotmail.com>
2021-08-03 19:07:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-document.body.addEventListener('keyup', (event)=>{
+document.body.addEventListener('keyup', (event) => {
     playSound(event.code.toLowerCase())
 })
 
-document.querySelector('.composer button').addEventListener('click', ()=>{
+
+document.querySelector('.composer button').addEventListener('click', () => {
     let song = document.querySelector('#input').value
 
-    if(song != ''){
+    if (song != '') {
         let songArray = song.split('')
         playComposition(songArray)
     }
 })
 
-function playSound(sound){
+
+
+
+function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`)
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
-    
-    if (audioElement){
+
+    if (audioElement) {
         audioElement.currentTime = 0
         audioElement.play()
     }
 
-    if(keyElement){
+    if (keyElement) {
         keyElement.classList.add('active')
 
-        setTimeout(()=>{
+        setTimeout(() => {
             keyElement.classList.remove('active')
         }, 300)
 
     }
 }
 
-function playComposition(songArray){
+function playComposition(songArray) {
     let wait = 0
-    for(let songItem of songArray){
-        setTimeout(()=>{
+    for (let songItem of songArray) {
+        setTimeout(() => {
             playSound(`key${songItem}`)
         }, wait)
 
